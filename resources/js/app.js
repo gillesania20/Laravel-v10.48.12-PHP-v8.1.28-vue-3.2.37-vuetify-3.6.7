7334bb6194c983514282f7feb0dbbf39d899797b
Lorenz Gillesania <lorenzgillesania20@gmail.com>
2024-05-30 10:16:35 +0100

    Added vuetify/styles

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,6 +8,7 @@ import './bootstrap';
 import { createApp } from 'vue';
 
 // Vuetify
+import 'vuetify/styles';
 import { createVuetify } from 'vuetify';
 import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
